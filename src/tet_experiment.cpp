065263fa4fb8245023d8992a04440ca4ca205597
max <maximilian.kohlbrenner@gmx.de>
2020-04-15 16:21:31 +0200

    Refactor

--- src/tet_experiment.cpp
+++ src/tet_experiment.cpp
@@ -38,77 +38,33 @@ typedef Kernel::Point_3 Point;
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/normal_distribution.hpp>    
 
-void solveDirichletProblem(CGALTriangulation<Kernel>& tri, Eigen::MatrixXd& x)
+void solveHeatProblem(CGALTriangulation<Kernel>& tri, Eigen::MatrixXd& h_fem, Eigen::MatrixXd& h_dec)
 {
     const int cntr = tri.centerVertex();
-    auto constr = tri.surfaceVertices();
-    
     const int n = tri.mesh.number_of_vertices();
     
-    constr.push_back(cntr);
-    
-    Eigen::SparseMatrix<double> A, L, L2, M;
+    Eigen::SparseMatrix<double> A_fem, A_dec, L_fem, L_dec, M;
     tri.massMatrix(M);
-    tri.FEMLaplacian(L);
-    tri.DECLaplacian(L2);
-    
-    {
-        constr.pop_back();
-        
-        std::ofstream file("../constr");
-        for(int i : constr) file << i << "\n";
-        file.close();
-        
-        Eigen::saveMarket(L, "../LFEM.mtx");
-        Eigen::saveMarket(L2, "../LDEC.mtx");
-    }
-    
+    tri.FEMLaplacian(L_fem);
+    tri.DECLaplacian(L_dec);
     
     const double t = tri.meanEdgeLengthSquared();
-    A = M + t * L;
+    A_fem = M + t * L_fem;
+	A_dec = M - t * L_dec; 
     
-    //Eigen::SparseMatrix<double> LTL = L.transpose() * L;
-    //Eigen::MatrixXd LTb = L.transpose() * b;
-    
-    Eigen::VectorXd bb(A.cols());
+    Eigen::VectorXd bb(A_fem.cols());
     bb.setZero();
     bb(cntr) = 1.;
     
-    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> chol;
-    chol.analyzePattern(A);
-    chol.factorize(A);
-    Eigen::VectorXd h = chol.solve(bb);
-    
-    {
-        double mi = h.minCoeff();
-        double ma = h.maxCoeff();
-        
-        for(int i = 0; i < h.size(); ++i)
-            h(i) = (h(i) - mi) / (ma - mi);
-        
-        x = h;
-    }
-    
-    //solveConstrainedSymmetric(A, b, constr, C, x);
-    
-    Eigen::MatrixXd pts;
-    tri.getPoints(pts);
-    
-    const double div = sqrt(- 4 * t * log(1e-20));
-    
-    std::ofstream file("../x");
-    
-    for(int i = 0; i < x.size(); ++i)
-    {
-        //    x(i) = (pts.row(i) - pts.row(cntr)).norm();
-        double y = sqrt(- 4 * t * log((1. - 1e-20) * x(i) + 1e-20)) / div;
-        file <<  (pts.row(i) - pts.row(cntr)).norm() << " " << x(i) << " " << y << "\n";
-        
-        x(i) = y;
-    }
-    
-    
-    file.close();
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> chol_fem;
+    chol_fem.analyzePattern(A_fem);
+    chol_fem.factorize(A_fem);
+    h_fem = chol_fem.solve(bb);
+
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> chol_dec;
+    chol_dec.analyzePattern(A_dec);
+    chol_dec.factorize(A_dec);
+    h_dec = chol_dec.solve(bb);
 }
 
 
@@ -131,7 +87,7 @@ void screenshot(igl::opengl::glfw::Viewer& viewer, std::string filename) {
 
 bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
 {
-  if (key == '1')
+  if (key == '2')
   {
 	/*
 	  std::cout << FILENAME_base << std::endl;
@@ -157,6 +113,36 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 	  */
   }
   
+}
+
+
+void setTexture(const std::string filename, igl::opengl::ViewerData& viewerData)
+{
+    static Eigen::Matrix<unsigned char, -1, -1> R, G, B;
+    static std::string currentFile;
+    
+    if(currentFile.compare(filename))
+    {
+        currentFile = filename;
+        
+        unsigned w, h;
+        std::vector<unsigned char> image; //the raw pixels
+        
+        if( lodepng::decode(image, w, h, filename) ) return;
+        
+        R.resize(w, h);
+        G.resize(w, h);
+        B.resize(w, h);
+        
+        for(int i = 0; i < w * h; ++i)
+        {
+            R.data()[i] = image[4 * i];
+            G.data()[i] = image[4 * i + 1];
+            B.data()[i] = image[4 * i + 2];
+        }
+    }
+    
+    viewerData.set_texture(R, G, B);
 }
 
 int 
@@ -197,9 +183,24 @@ loadMeshWithOrbitpoints(CGALTriangulation<Kernel> &tri, std::vector<int> &orbiti
 	return 1;
 }
 
+Eigen::MatrixXd  normalizeHeatValues(Eigen::MatrixXd h) {
+
+	Eigen::MatrixXd h_normed;
+	h_normed.resize(h.rows(), 1);
+	double mi = h.minCoeff();
+	double ma = h.maxCoeff();
+	for(int i = 0; i < h.size(); ++i)
+		h_normed(i) = (h(i) - mi) / (ma - mi);
+	return h_normed;
+}
+
 
 int main(int argc, char *argv[])
 {
+
+	// no gui output
+	bool silent = true;
+
 	CGALTriangulation<Kernel> tri;
 	int originind;
 	std::vector<int> orbitinds;
@@ -226,7 +227,6 @@ int main(int argc, char *argv[])
 	std::vector<int>  faceids; 
 	Metric metric_shown;
 
-
 	std::cout << "Calc metrics" << std::endl;
 	metric_shown = minangle;
 	std::map<Metric, Eigen::VectorXd> cell_metrics;
@@ -252,10 +252,6 @@ int main(int argc, char *argv[])
 		for (int i=0; i < cell_metrics[amips].size(); i++) cell_metrics[amips][i] = (cell_metrics[amips][i] - 3) / amips_max;
 	} 
 
-
-	/* ################## SHOW  MESH ####################*/
-
-
 	Eigen::MatrixXd cellcolors_volume; 
 	igl::colormap(igl::COLOR_MAP_TYPE_VIRIDIS, cell_metrics[volume], true, cellcolors_volume);
 	cellcolors[volume] = cellcolors_volume;
@@ -266,118 +262,192 @@ int main(int argc, char *argv[])
 	igl::colormap(igl::COLOR_MAP_TYPE_VIRIDIS, cell_metrics[amips], normalize, cellcolors_amips);
 	cellcolors[amips] = cellcolors_amips;
 
-    Point origin = Point(0., 0., 0.);
+	/* ################## HEAT DIFFUSION ################ */
 
-    Eigen::MatrixXd x;
-    x.resize(tri.mesh.number_of_vertices(), 1);
-    x.setZero();
-  //  solveDirichletProblem(tri, x);
-    
-    std::cout << "done" << std::endl;
+    Eigen::MatrixXd x, h_fem, h_dec, h;
+	solveHeatProblem(tri, h_fem, h_dec);
+	std::cout << "The heat: " << std::endl;
+	//std::cout << h << std::endl;
+    //std::cout << "done" << std::endl;
+	
+	for (int i = 0; i < h_fem.size(); i++) {
+		std::cout << h_fem(i) << " " << h_dec(i) << std::endl;	
+	}
+
+	// normalize the heat values:
+	x = normalizeHeatValues(h_dec);
+
+	if (!silent) {
+		/* ################## SHOW  MESH ####################*/
+
+		Eigen::MatrixXd V;
+		Eigen::MatrixXi F;
+
+		double offset = 0.1;
+
+		std::array<double, 4> plane{1,0,0,offset};
+		std::vector<std::vector<int>> cmreturn = tri.cutMesh(plane, V, F);
+		std::vector<int>  ids     = cmreturn[0];
+		faceids = cmreturn[1];
+		facecolors.resize(faceids.size(), 3);
+		for (int i; i < faceids.size(); i++) facecolors.row(i) = cellcolors[metric_shown].row(faceids[i]);
+
+		/*
+		Eigen::VectorXd x2();
+		for(int i = 0; i < ids.size(); ++i)
+			x2(i) = x(ids[i]);
 
 
-	Eigen::MatrixXd V;
-	Eigen::MatrixXi F;
+		Eigen::MatrixXd color;
+		igl::colormap(igl::COLOR_MAP_TYPE_INFERNO, x2, false, color);
+		viewer.data().set_colors(color);
+		*/
 
-	double offset = 0.1;
 
-	std::array<double, 4> plane{1,0,0,offset};
-	std::vector<std::vector<int>> cmreturn = tri.cutMesh(plane, V, F);
-	std::vector<int>  ids     = cmreturn[0];
-	faceids = cmreturn[1];
-	facecolors.resize(faceids.size(), 3);
-	for (int i; i < faceids.size(); i++) facecolors.row(i) = cellcolors[metric_shown].row(faceids[i]);
+		// Style
+		Eigen::Vector3d ambient(.1,.1,.1);
+		Eigen::Vector3d diffuse(.7,.7,.7);
+		Eigen::Vector3d specular(.9,.9,.9);
 
-	// Style
-	Eigen::Vector3d ambient(.1,.1,.1);
-	Eigen::Vector3d diffuse(.7,.7,.7);
-	Eigen::Vector3d specular(.9,.9,.9);
+		// Init the viewer
+		igl::opengl::glfw::Viewer viewer;
 
-	// Init the viewer
-	igl::opengl::glfw::Viewer viewer;
+		// Attach a menu plugin
+		igl::opengl::glfw::imgui::ImGuiMenu menu;
+		viewer.plugins.push_back(&menu);
 
-	// Attach a menu plugin
-	igl::opengl::glfw::imgui::ImGuiMenu menu;
-	viewer.plugins.push_back(&menu);
+		// Customize the menu
+		float iso = 0.5f;
+		bool orientation = false;
+		int dir = 0;
 
-	// Customize the menu
-	float iso = 0.5f;
-	bool orientation = false;
-	int dir = 0;
+		Metric metric = minangle;
+		static bool showValues = false;
+		static bool showHeat   = true;
 
-	Metric metric = minangle;
-	static bool showValues = false;
-
-	// Add content to the default menu window
-	menu.callback_draw_viewer_menu = [&]()
-	{
-		// Draw parent menu content
-		
-		if (ImGui::CollapsingHeader("Presentation", ImGuiTreeNodeFlags_DefaultOpen))
+		// Add content to the default menu window
+		menu.callback_draw_viewer_menu = [&]()
 		{
-			Metric oldmetric = metric_shown;
-			ImGui::Combo("Metric", (int *)(&metric_shown), "MinAngle\0AMIPS\0Volume\0\0");
-
-			/*
-			if(ImGui::Checkbox("Show Metric Values", &showValues)) {
-				for(int i = 0; i < F.rows(); ++i) {
-					const Eigen::Vector3d FaceCenter( (V(F(i,0), 0)+ V(F(i,1), 0)+ V(F(i,2), 0))/ 3.,  (V(F(i,0), 1)+ V(F(i,1), 1)+ V(F(i,2), 1))/ 3.,  (V(F(i,0), 2)+ V(F(i,1), 2)+ V(F(i,2), 2))/ 3.);
-					viewer.data().add_label(FaceCenter,std::to_string(cell_metrics[metric][faceids[i]]));
-				}
-			} 
-			*/
-			//else {
-			//	viewer.data().clear_labels();	
-			//}
-
-			if (oldmetric != metric_shown){
-				facecolors.resize(faceids.size(), 3);
-				for (int i; i < faceids.size(); i++) facecolors.row(i) = cellcolors[metric_shown].row(faceids[i]);
-				viewer.data().set_colors(facecolors);
-			}
-		}
-		
-		// Add new group
-		if (ImGui::CollapsingHeader("Cut View", ImGuiTreeNodeFlags_DefaultOpen))
-		{
-			double mi = -1.;
-			double ma = 1.;
-			double oldoffset = offset;
-			ImGui::DragScalar("cut offset", ImGuiDataType_Double, &offset, 0.1, &mi, &ma, "%.4f");
+			// Draw parent menu content
 			
-			int oldDir = dir;
-			ImGui::Combo("Direction", (int *)(&dir), "X\0Y\0Z\0\0");
-			
-			if(oldoffset != offset || dir != oldDir)
+			if (ImGui::CollapsingHeader("Presentation", ImGuiTreeNodeFlags_DefaultOpen))
 			{
-				std::array<double, 4> plane{0,0,0,offset};
-				plane[dir] = 1;
-				
-				Eigen::MatrixXd V;
-				Eigen::MatrixXi F;
-				
-				cmreturn = tri.cutMesh(plane, V, F);
-				ids = cmreturn[0];
-				faceids = cmreturn[1];
-				
-				if(ids.size())
-				{
-					viewer.data().clear();
-					viewer.data().set_mesh(V, F);
+				Metric oldmetric = metric_shown;
+				ImGui::Combo("Metric", (int *)(&metric_shown), "MinAngle\0AMIPS\0Volume\0\0");
 
+				/*
+				if(ImGui::Checkbox("Show Metric Values", &showValues)) {
+					for(int i = 0; i < F.rows(); ++i) {
+						const Eigen::Vector3d FaceCenter( (V(F(i,0), 0)+ V(F(i,1), 0)+ V(F(i,2), 0))/ 3.,  (V(F(i,0), 1)+ V(F(i,1), 1)+ V(F(i,2), 1))/ 3.,  (V(F(i,0), 2)+ V(F(i,1), 2)+ V(F(i,2), 2))/ 3.);
+						viewer.data().add_label(FaceCenter,std::to_string(cell_metrics[metric][faceids[i]]));
+					}
+				} 
+				*/
+				//else {
+				//	viewer.data().clear_labels();	
+				//}
+
+				if (oldmetric != metric_shown){
 					facecolors.resize(faceids.size(), 3);
 					for (int i; i < faceids.size(); i++) facecolors.row(i) = cellcolors[metric_shown].row(faceids[i]);
 					viewer.data().set_colors(facecolors);
 				}
-			}
-		}
-	};
 
-	// Plot the mesh
-	viewer.data().set_mesh(V, F);
-	viewer.data().set_colors(facecolors);
-	viewer.core().background_color.setOnes();
-	viewer.callback_key_down = &key_down;
+			}
+			
+			// Add new group
+			if (ImGui::CollapsingHeader("Cut View", ImGuiTreeNodeFlags_DefaultOpen))
+			{
+				double mi = -1.;
+				double ma = 1.;
+				double oldoffset = offset;
+				ImGui::DragScalar("cut offset", ImGuiDataType_Double, &offset, 0.1, &mi, &ma, "%.4f");
+				
+				int oldDir = dir;
+				ImGui::Combo("Direction", (int *)(&dir), "X\0Y\0Z\0\0");
+				
+				if(oldoffset != offset || dir != oldDir)
+				{
+					std::array<double, 4> plane{0,0,0,offset};
+					plane[dir] = 1;
+					
+					Eigen::MatrixXd V;
+					Eigen::MatrixXi F;
+					
+					cmreturn = tri.cutMesh(plane, V, F);
+					ids = cmreturn[0];
+					faceids = cmreturn[1];
+					
+					if(ids.size())
+					{
+						viewer.data().clear();
+						viewer.data().set_mesh(V, F);
+
+						if (!showHeat) {
+							facecolors.resize(faceids.size(), 3);
+							for (int i; i < faceids.size(); i++) facecolors.row(i) = cellcolors[metric_shown].row(faceids[i]);
+							viewer.data().set_colors(facecolors);
+						} else {
+
+						Eigen::VectorXd x2(ids.size());
+						for(int i = 0; i < ids.size(); ++i)
+							x2(i) = x(ids[i]);
+
+
+						Eigen::MatrixXd color;
+						igl::colormap(igl::COLOR_MAP_TYPE_INFERNO, x2, false, color);
+						viewer.data().set_colors(color);
+						
+						}
+
+						/*
+						viewer.data().clear();
+						viewer.data().set_mesh(V, F);
+						viewer.data().uniform_colors(ambient, diffuse, specular);
+						viewer.data().show_texture = 1;
+						
+						Eigen::VectorXd x2(ids.size());
+						for(int i = 0; i < ids.size(); ++i)
+							x2(i) = x(ids[i]);
+						
+						
+						setTexture("../data/tex.png", viewer.data(cutMeshId));
+						Eigen::MatrixXd UV(x2.rows(), 2);
+						UV.col(0) = UV.col(1) = x2;
+						viewer.data(cutMeshId).set_uv(UV);
+						*/
+
+
+					}
+				}
+			}
+		};
+
+		// Plot the mesh
+		viewer.data().set_mesh(V, F);
+		viewer.data().set_colors(facecolors);
+
+		viewer.core().background_color.setOnes();
+		viewer.callback_key_down = &key_down;
+
+		/*
+		if (!showHeat) {
+			viewer.data().set_colors(facecolors);
+		} else {
+
+		Eigen::VectorXd x2(ids.size());
+		for(int i = 0; i < ids.size(); ++i)
+			x2(i) = x(ids[i]);
+
+
+		Eigen::MatrixXd color;
+		igl::colormap(igl::COLOR_MAP_TYPE_INFERNO, x2, false, color);
+		viewer.data().set_colors(color);
 		
-	viewer.launch();
+		}
+		*/
+
+			
+		viewer.launch();
+	}
 }
